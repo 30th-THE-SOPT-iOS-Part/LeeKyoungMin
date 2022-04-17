7f72749fb39eccd81c74a53750d157bffbe7972f
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-04-17 20:25:12 +0900

    [#18] 2주차 도전과제 - Tabbar color 변경

--- assignment/assignment/Scene/MainTabbarController.swift
+++ assignment/assignment/Scene/MainTabbarController.swift
@@ -11,28 +11,22 @@ final class MainTabbarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setTabbarUI()
+        setTabbar()
     }
     
-    private func setTabbarUI() {
-        UITabBar.clearShadow()
-        if #available(iOS 15.0, *) {
-            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
-        }
+    private func setTabbar() {
+        var borderView = UIView(frame: CGRect(x: 0, y: 0,
+                                              width: UIScreen.main.bounds.width, height: 1.5))
+        borderView.backgroundColor = UIColor(red: 218.0/255.0, green: 218.0/255.0, blue: 218.0/255.0, alpha: 1.0)
+        self.tabBar.addSubview(borderView)
+        
         self.tabBar.tintColor = .black
         self.tabBar.backgroundColor = .white
-        self.tabBar.layer.shadowColor = UIColor(red: 218.0/255.0, green: 218.0/255.0, blue: 218.0/255.0, alpha: 1.0).cgColor
-        self.tabBar.layer.shadowOpacity = 1
-        self.tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.tabBar.layer.shadowRadius = 1.5
     }
     
-}
-
-extension UITabBar {
-    static func clearShadow() {
-        UITabBar.appearance().shadowImage = UIImage()
-        UITabBar.appearance().backgroundImage = UIImage()
-        UITabBar.appearance().backgroundColor = UIColor.white
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        tabBar.backgroundColor = item.tag == 3 ? .black : .white
+        tabBar.tintColor = item.tag == 3 ? .white : .black
+        tabBar.barTintColor = item.tag == 3 ? .white : .black
     }
 }
