7ba9fd7ca9307817bba1c2a696f4bd412eef29e6
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-04-29 15:51:47 +0900

    [#14] add Image Consts

--- assignment/assignment/Configuration/Const/Image.swift
+++ assignment/assignment/Configuration/Const/Image.swift
@@ -40,5 +40,37 @@ extension Const {
         static let icnPasswordHidden = UIImage(named: "icn_password_hidden")!
         static let icn_password_shown = UIImage(named: "icn_password_shown")!
         
+        // MARK: - Home Tab icon
+        
+        static let icn_add = UIImage(named: "icn_add")!
+        static let icn_bookmark = UIImage(named: "icn_bookmark")!
+        static let icn_comment = UIImage(named: "icn_comment")!
+        static let icn_unlike = UIImage(named: "icn_unlike")!
+        static let icn_like = UIImage(named: "icn_like")!
+        static let icn_more = UIImage(named: "icn_more")!
+        static let icn_share = UIImage(named: "icn_share")!
+        
+        // MARK: - dummy picture datas
+        
+        static let kuromi1 = UIImage(named: "kuromi1")!
+        static let kuromi2 = UIImage(named: "kuromi2")!
+        static let kuromi3 = UIImage(named: "kuromi3")!
+        static let kuromi4 = UIImage(named: "kuromi4")!
+        static let kuromi5 = UIImage(named: "kuromi5")!
+        static let kuromi6 = UIImage(named: "kuromi6")!
+        static let kuromi7 = UIImage(named: "kuromi7")!
+        static let kuromi8 = UIImage(named: "kuromi8")!
+        static let kuromi9 = UIImage(named: "kuromi9")!
+        static let kuromi10 = UIImage(named: "kuromi10")!
+        static let kuromi11 = UIImage(named: "kuromi11")!
+        static let kuromi12 = UIImage(named: "kuromi12")!
+        static let kuromi13 = UIImage(named: "kuromi13")!
+        static let kuromi14 = UIImage(named: "kuromi14")!
+        static let kuromi15 = UIImage(named: "kuromi15")!
+        static let kuromi16 = UIImage(named: "kuromi16")!
+        static let kuromi17 = UIImage(named: "kuromi17")!
+        static let kuromi18 = UIImage(named: "kuromi18")!
+        static let kuromi19 = UIImage(named: "kuromi19")!
+        static let kuromi20 = UIImage(named: "kuromi20")!
     }
 }
